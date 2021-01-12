6fbf282bdff4db645a9fb4bd30a9a18b8595e688
mack millar <mackmillar@gmail.com>
2021-01-12 15:17:25 +0000

    created function for total pop

--- src/app.js
+++ src/app.js
@@ -3,6 +3,34 @@ import Vue from 'vue';
 document.addEventListener('DOMContentLoaded', function(){
     new Vue({
         el: '#app',
+        data: {
+            totalPop: 0,
+            countriesData: []
+        },
+        mounted(){
+            this.fetchTotalPop();
+        
+          },
+        computed: {
 
-    });
+            totalPopulation: function(){
+                return this.countriesData.reduce((total, country) => {
+                    return total + country.population;
+            
+                }, 0);
+
+        },},
+        
+        
+        
+        methods: {
+            fetchTotalPop: function() {
+                fetch("https://restcountries.eu/rest/v2/all")
+                    .then(response => response.json())
+                    .then(data => this.countriesData = data);
+                    
+            }
+        }
+
+        });
 });
